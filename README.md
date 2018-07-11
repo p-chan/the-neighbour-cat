260bf8dbf2f2c98ea9f8302488afc3f1598e6ace
P-Chan <hello@0x50.io>
2018-07-12 07:46:01 +0900

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# The Neighbour Cat
+
+> The Neighbour Cat (*近所の猫*) is a fuckin' awesome knowledgeable bot
---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const Botkit = require('botkit')
+const dotenv = require('dotenv')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const controller = Botkit.slackbot({
+  debug: false
+})
+
+const bot = controller.spawn({
+  token: process.env.SLACK_TOKEN
+}).startRTM()
+
+controller.hears('ping', 'direct_mention', (bot, message) => {
+  bot.reply(message, 'pong')
+})
