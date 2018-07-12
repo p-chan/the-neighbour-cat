d4d1bb525dc7278747545dd9c11a8e02379dbc86
P-Chan <hello@0x50.io>
2018-07-12 09:52:52 +0900

    chore: Add ESLint and Prettier

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ const controller = Botkit.slackbot({
   debug: false
 })
 
-const bot = controller.spawn({
-  token: process.env.SLACK_TOKEN
-}).startRTM()
+const bot = controller
+  .spawn({
+    token: process.env.SLACK_TOKEN
+  })
+  .startRTM()
 
 controller.hears('ping', 'direct_mention', (bot, message) => {
   bot.reply(message, 'pong')
